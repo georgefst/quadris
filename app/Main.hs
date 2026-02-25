b80db1dff270e52a89194409bb1ec3141d6cc142
George Thomas <georgefsthomas@gmail.com>
2026-02-25 10:09:23 +0000

    Force stylesheet reloading

--- app/Main.hs
+++ app/Main.hs
@@ -8,6 +8,7 @@ import Quadris (Opts (..), opts)
 import Quadris.Miso
 import Miso
 import System.Environment
+import System.Random.Stateful (globalStdGen, uniformM)
 
 {- FOURMOLU_DISABLE -}
 main :: IO ()
@@ -23,11 +24,19 @@ main = do
         -- =<< const (pure Nothing)
         -- TODO catch failures here, e.g. for when the model type has changed
         =<< lookupStore foreignStoreId
+    -- TODO this is a hack to ensure the stylesheet is reloaded on GHCI reload
+    -- it's not about the HTTP cache - browsers don't even make a new request since the DOM element hasn't changed
+    -- and if this is our long-term solution, it should really be a counter or timestamp to avoid potential collisions
+    cacheBuster <- ms <$> uniformM @Word globalStdGen
+    -- TODO we should find some way to _not_ request a new stylesheet when it hasn't changed
+    -- one way would be if GHCIWatch could run different commands depending on what file triggered the reload
+    -- for now we can uncomment the line below, though that will only work from the second reload onwards
+    -- let cacheBuster = ""
     -- TODO it'd be simpler if we could write to the store on unmount only, instead of every update
     -- and in theory more efficient, at least for huge models
     -- but the unmount action doesn't get run on reload
     -- and actually, given that it can't do IO directly, we'd need a new action as well...
-    let a = startApp defaultEvents $ (app foreignStoreId model){styles = [Href "assets/style.css"]}
+    let a = startApp defaultEvents $ (app foreignStoreId model){styles = [Href $ "assets/style.css#" <> cacheBuster]}
     getProgName >>= \case
         "<interactive>" -> reload a
         _ -> a
